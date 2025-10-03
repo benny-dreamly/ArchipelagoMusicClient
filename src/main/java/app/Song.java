594a9251154b30e9d025b66607c1bb44b3290590
Benny Dreamly <bgaming0277@gmail.com>
2025-10-03 14:35:58 -0600

    start song class

--- src/main/java/app/Song.java
+++ src/main/java/app/Song.java
@@ -1,4 +1,20 @@
 package app;
 
 public class Song {
+
+    private final String title;
+    private final String type;
+
+    public Song(String title, String type) {
+        this.title = title;
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
