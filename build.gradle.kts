d482fe9af9b9dd366cc092661d89d19550148a80
Benny Dreamly <bgaming0277@gmail.com>
2025-10-03 14:46:25 -0600

    add other dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.openjfx:javafx-controls:24.0.0.1")
+    implementation("org.openjfx:javafx-media:24.0.0.1")
 }
 
 tasks.test {
