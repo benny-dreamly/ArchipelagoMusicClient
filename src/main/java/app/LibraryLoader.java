82ef34b5fc9be8eed1c91e54f6d22bb2f4623f7c
Benny Dreamly <bgaming0277@gmail.com>
2025-10-03 15:39:26 -0600

    json loading nice

--- src/main/java/app/LibraryLoader.java
+++ src/main/java/app/LibraryLoader.java
@@ -4,15 +4,24 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.util.List;
 
 public class LibraryLoader {
 
-    public List<SongJSON> loadSongs(String filePath) throws Exception {
+    public List<SongJSON> loadSongs(String resourcePath) throws Exception {
         Gson gson = new Gson();
         Type listType = new TypeToken<List<SongJSON>>() {}.getType();
-        try (FileReader reader = new FileReader(filePath)) {
+
+        // Load JSON from resources
+        InputStream is = getClass().getResourceAsStream(resourcePath);
+        if (is == null) {
+            throw new IllegalArgumentException("Resource not found: " + resourcePath);
+        }
+
+        try (InputStreamReader reader = new InputStreamReader(is)) {
             return gson.fromJson(reader, listType);
         }
     }
