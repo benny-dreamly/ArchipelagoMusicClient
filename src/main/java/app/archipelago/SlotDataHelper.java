82b75a0a0e5f500572b2919850f4197a81fe256b
Benny Dreamly <bgaming0277@gmail.com>
2025-10-19 16:06:22 -0600

    add a WIP slot data parser

---
+++ src/main/java/app/archipelago/SlotDataHelper.java
@@ -0,0 +1,76 @@
+package app.archipelago;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.*;
+
+public class SlotDataHelper {
+
+    private static Map<String, SlotOption> slotOptions = null;
+
+    public static void loadSlotOptions(File configDir) {
+        File file = new File(configDir, "slot_data.json");
+        if (!file.exists()) {
+            System.err.println("No slot_data.json found in " + configDir.getAbsolutePath());
+            slotOptions = Collections.emptyMap();
+            return;
+        }
+
+        try (FileReader reader = new FileReader(file)) {
+            Type type = new TypeToken<Map<String, SlotOption>>() {}.getType();
+            slotOptions = new Gson().fromJson(reader, type);
+            System.out.println("Loaded slot_data.json with " + slotOptions.size() + " keys.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            slotOptions = Collections.emptyMap();
+        }
+    }
+
+    public static Set<String> getEnabledAlbums(Map<String, Object> slotData) {
+        Set<String> enabled = new HashSet<>();
+        if (slotOptions == null) return enabled;
+
+        for (Map.Entry<String, SlotOption> entry : slotOptions.entrySet()) {
+            String key = entry.getKey();
+            SlotOption option = entry.getValue();
+
+            Object val = slotData.get(key);
+            if (val instanceof Number && ((Number) val).intValue() == 1) {
+                if ("album".equals(option.type)) {
+                    enabled.add(option.display_name);
+                }
+            }
+        }
+
+        return enabled;
+    }
+
+    public static Set<String> getEnabledCategories(Map<String, Object> slotData) {
+        Set<String> enabled = new HashSet<>();
+        if (slotOptions == null) return enabled;
+
+        for (Map.Entry<String, SlotOption> entry : slotOptions.entrySet()) {
+            String key = entry.getKey();
+            SlotOption option = entry.getValue();
+
+            Object val = slotData.get(key);
+            if (val instanceof Number && ((Number) val).intValue() == 1) {
+                if ("song_category".equals(option.type)) {
+                    enabled.add(option.display_name);
+                }
+            }
+        }
+
+        return enabled;
+    }
+
+    public static class SlotOption {
+        public String type;
+        public String display_name;
+    }
+}
