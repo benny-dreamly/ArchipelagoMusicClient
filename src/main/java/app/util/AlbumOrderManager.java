0e8898616beb65a204f86743dc05d9912cfa8d58
Benny Dreamly <bgaming0277@gmail.com>
2025-11-25 10:07:15 -0700

    replace .* imports in the util files

--- src/main/java/app/util/AlbumOrderManager.java
+++ src/main/java/app/util/AlbumOrderManager.java
@@ -6,7 +6,12 @@ import com.google.gson.reflect.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
