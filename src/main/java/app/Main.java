4add52382407abb0cbbb91cb0e34a2b80e766919
Benny Dreamly <bgaming0277@gmail.com>
2026-08-17 16:11:10 -0600

    add timestamped log files in logs/ directory, one per run

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
+        File logDir = new File("logs");
+        logDir.mkdirs();
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+        String logFile = new File(logDir, "MusicAppDemo-" + timestamp + ".log").getAbsolutePath();
+        System.setProperty("org.slf4j.simpleLogger.logFile", logFile);
+
         MusicAppDemo.main(args);
     }
 }
