48c17626a9f91aca26beb5ac6585eea14006d8c5
Benny Dreamly <bgaming0277@gmail.com>
2026-09-23 21:00:01 -0600

    scaffold standalone Android phone client project

---
+++ mobile/app/src/main/java/archipelago/phone/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package archipelago.phone.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import archipelago.phone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
